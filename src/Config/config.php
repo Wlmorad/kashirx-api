6e7466a11ef37733fd4df2db2153c0236c7cddfb
Wael Murad <wlmorad@gmail.com>
2025-06-06 20:04:26 +0300

    Initial OOP API setup

---
+++ src/Config/config.php
@@ -0,0 +1,17 @@
+<?php
+// src/Config/config.php
+
+return [
+    'app_name'    => 'KashirX',
+    'version'     => '1.0.0',
+    'developer'   => 'Wael Murad',
+    'powered_by'  => 'Platista',
+    'base_path'   => '/api-kashirx', // المسار الأساسي
+    'db' => [
+        'host'     => 'localhost',
+        'dbname'   => 'pos_system',
+        'user'     => 'root',
+        'password' => '',
+        'charset'  => 'utf8mb4',
+    ]
+];
